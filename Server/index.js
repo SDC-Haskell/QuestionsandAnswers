c446ba337f4903c8c1834867eca262c22f4d444d
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-03-18 12:51:09 -0700

    Completed basic api routes for both get requests

--- Server/index.js
+++ Server/index.js
@@ -26,9 +26,31 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello there'));
 
-// Testing our database
+app.get('/qa/questions', ((req,res) => {
+  /*
+    Gets a specific product ID from the database and returns
+    the questions object containing all of the answers for each question (and photos)
 
+    The number of questions (and pages of questions) returned are specified by the page and count query params
+
+    This is going to require multiple layers of queries
+
+    current productid = 17072
+  */
+  console.log(req.query);
+
+  res.send(req.query.product_id);
+
+}));
+
+app.get('/qa/questions/:question_id/answers', ((req,res) => {
+  console.log(req.params);
+  res.send(req.params);
+}));
+
+// Testing our database
+// sudo kill -9 `sudo lsof -t -i:5000`
 
 //models.sequelize.sync().then(() => {
-app.listen(PORT, console.log(`Listening on port ${PORT}`))
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
 //});
